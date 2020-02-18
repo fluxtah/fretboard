50547bcaeff42986ee5cf720bb30fe8f1e974e4b
Ian Warwick <ian.warwick@just-eat.com>
2020-02-18 10:39:49 +0000

    updated example app

--- fretboard-sample/src/main/java/com/citizenwarwick/fretboard/example/MainActivity.kt
+++ fretboard-sample/src/main/java/com/citizenwarwick/fretboard/example/MainActivity.kt
@@ -6,8 +6,7 @@ import androidx.ui.core.setContent
 import androidx.ui.layout.Container
 import androidx.ui.material.MaterialTheme
 import com.citizenwarwick.fretboard.GuitarChord
-import com.citizenwarwick.fretboard.Marker.FrettedNote
-import com.citizenwarwick.fretboard.Marker.Mute
+import com.citizenwarwick.fretboard.fingering
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,15 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContent {
             MaterialTheme {
                 Container {
-                    val fingers = listOf(
-                        FrettedNote(1, 2),
-                        FrettedNote(2, 3),
-                        FrettedNote(3, 2),
-                        FrettedNote(4, 0),
-                        Mute(5),
-                        Mute(6)
-                    )
-                    GuitarChord(fingers)
+                    GuitarChord("2|3|2|0|x|x".fingering)
                 }
             }
         }
